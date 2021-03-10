7482dd0f598a0994df33f271062a2e510c9b0ba8
Joe Spraggon <joe_spraggon@hotmail.co.uk>
2021-03-10 17:52:14 +0000

    passing test: #update_quality - normal items quailty decrease by 2 after sell by date

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -8,6 +8,7 @@ describe GildedRose do
     test_item_list = Array.new.push(test_item1, test_item2)
   }
 
+
   subject {described_class.new(test_item_list)}
 
   it 'initialize a GildedRose object with items' do
@@ -17,12 +18,19 @@ describe GildedRose do
 
   context '#update_quality' do
     
-    it 'decreases a normal items sell_in & quality' do
+    it 'normal items sell_in & quality decrease by 1' do
       subject.update_quality
       expect(subject.items[0].sell_in).to eq(9)
       expect(subject.items[0].quality).to eq(19)
       expect(subject.items[1].sell_in).to eq(4)
       expect(subject.items[1].quality).to eq(39)
+    end
+
+    it 'normal items quality decreases by 2 after sell by date' do
+      5.times{subject.update_quality}
+      expect(subject.items[1].quality).to eq(35)
+      subject.update_quality
+      expect(subject.items[1].quality).to eq(33)
     end
 
   end
