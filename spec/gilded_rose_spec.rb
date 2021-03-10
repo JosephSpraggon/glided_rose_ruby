295a62710bd023b8e08680e6b55834d2e2ac2569
JosephSpraggon <joe_spraggon@hotmail.co.uk>
2021-03-10 20:53:03 +0000

    passing tests: #update_quality - quality is never negative, 'Age Brie'increases in quality, quality is never over 50

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -5,7 +5,8 @@ describe GildedRose do
   let (:test_item_list){
     test_item1 = Item.new("test item1",10, 20)
     test_item2 = Item.new("test item2",5, 40)
-    test_item_list = Array.new.push(test_item1, test_item2)
+    aged_brie = Item.new("Aged Brie", 30, 20)
+    test_item_list = Array.new.push(test_item1, test_item2, aged_brie)
   }
 
 
@@ -32,6 +33,25 @@ describe GildedRose do
       subject.update_quality
       expect(subject.items[1].quality).to eq(33)
     end
+
+    it 'quality of an item is never negative' do
+      100.times{subject.update_quality}
+      expect(subject.items[0].quality).to eq(0)
+      expect(subject.items[1].quality).to eq(0)
+    end
+
+    it '"Aged Bried" actually increases in Quality the older it gets' do
+      30.times{subject.update_quality}
+      expect(subject.items[2].sell_in).to eq(0)
+      expect(subject.items[2].quality).to eq(50)
+    end
+
+    it 'quality of an item is never over 50' do
+      100.times{subject.update_quality}
+      expect(subject.items[2].quality).to eq(50)
+    end
+
+    
 
   end
 
