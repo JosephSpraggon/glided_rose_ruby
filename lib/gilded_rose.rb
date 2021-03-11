784c88bb23c24dd304708b9dc93b27e9595f73bc
Joe Spraggon <joe_spraggon@hotmail.co.uk>
2021-03-11 15:11:48 +0000

    swapping tenary statements to if/then, for readabilty, all tests pass

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -11,20 +11,17 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      if !SPECIAL_ITEMS.include?(item.name) && item.quality > 0
-        item.quality = item.quality - 1
+      if !SPECIAL_ITEMS.include?(item.name) && item.quality > 0 then item.quality = item.quality - 1
       else
         if item.quality < 50
           item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert" && item.sell_in < 11 && item.quality < 50
-              item.quality = item.quality + 1
-            if item.sell_in < 6 && item.quality < 50
-              item.quality = item.quality + 1
+          if item.name == "Backstage passes to a TAFKAL80ETC concert" && item.sell_in < 11 && item.quality < 50 then item.quality = item.quality + 1
+            if item.sell_in < 6 && item.quality < 50 then item.quality = item.quality + 1
             end
           end
         end
       end
-      if item.name != "Sulfuras, Hand of Ragnaros" ? item.sell_in = item.sell_in - 1 : false
+      if item.name != "Sulfuras, Hand of Ragnaros" then item.sell_in = item.sell_in - 1
       end
       if item.sell_in < 0
         if item.name != "Aged Brie"
@@ -38,7 +35,7 @@ class GildedRose
             item.quality = item.quality - item.quality
           end
         else
-          if item.quality < 50 ? item.quality = item.quality + 1 : nil
+          if item.quality < 50 then item.quality = item.quality + 1
           end
         end
       end
